c74b7f0793511e5bd357398b66e6ffd4e4d402be
Andy Litzinger <andy.litzinger@gmail.com>
2011-10-11 20:30:51 -0700

    Tests are failing! Help me any-one- you're my only hope

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,14 @@
+describe "Strings" do
+  context "when calling strip" do
+
+    it "should remove all whitespace from the beginning and the end of the string" do
+
+        lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
+
+        lyrics.strip.should eq "Hello, is it me you're looking for I can see it  in your eyes"
+
+  end
+  end
+
+end
+
